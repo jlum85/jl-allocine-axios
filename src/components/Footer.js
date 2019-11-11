49c9a8daed11de4963a8093d39b8d0d25a9d4627
jlum85 <jlum@wanadoo.fr>
2019-11-11 20:32:48 +0100

    correction footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,21 @@ import React from "react";
 const Footer = props => {
   return (
     <footer>
-      <span>
-        <svg
-          onClick={() => props.onChangePage(-1)}
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#000000"
-          strokeWidth="3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M11 17l-5-5 5-5M18 17l-5-5 5-5" />
-        </svg>
-      </span>
+      <svg
+        onClick={() => props.onChangePage(-1)}
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#000000"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M11 17l-5-5 5-5M18 17l-5-5 5-5" />
+      </svg>
+
       <span>
         Page {props.numPage} / {props.countPage}
       </span>
